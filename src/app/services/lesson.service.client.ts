7dfe9e72263a1281177346d4f640318204268b37
parshva45 <parshva45@gmail.com>
2018-06-20 18:05:47 -0400

    updating java server api urls

--- src/app/services/lesson.service.client.ts
+++ src/app/services/lesson.service.client.ts
@@ -1,6 +1,6 @@
 export class LessonServiceClient {
   findLessonsForModule(courseId, moduleId) {
-    return fetch('http://localhost:8080/api/course/' + courseId + '/module/' + moduleId + '/lesson')
+    return fetch('https://webdev-parshva-shah.herokuapp.com/api/course/' + courseId + '/module/' + moduleId + '/lesson')
       .then(response => response.json());
   }
 }
--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -1,6 +1,6 @@
 export class WidgetServiceClient {
   findWidgetsForLesson(lessonId) {
-    return fetch('http://localhost:8080/api/lesson/' + lessonId + '/widget')
+    return fetch('https://webdev-parshva-shah.herokuapp.com/api/lesson/' + lessonId + '/widget')
       .then(response => response.json());
   }
 }
